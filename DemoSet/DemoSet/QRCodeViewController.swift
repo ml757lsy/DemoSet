9e9732fabbb68bd4fffc0e449612f9bc74c151d3
LiShiyuan <ml757lsy@126.com>
2018-06-15 20:11:44 +0800

    修改 二维码识别区域标记

--- DemoSet/DemoSet/QRCodeViewController.swift
+++ DemoSet/DemoSet/QRCodeViewController.swift
@@ -13,7 +13,8 @@ import AVFoundation
 class QRCodeViewController: BaseViewController,AVCaptureMetadataOutputObjectsDelegate {
 
     private let session = AVCaptureSession()
-    
+    private var isShow:Bool = false//显示标签
+    private let box = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         customNavigationBar()
@@ -78,8 +79,43 @@ class QRCodeViewController: BaseViewController,AVCaptureMetadataOutputObjectsDel
         layer?.frame = CGRect.init(x: 30, y: 100, width: view.width-60, height: view.height-300)
         view.layer.insertSublayer(layer!, at: 0)
         
+        view.addSubview(box)
+        box.frame = CGRect.init(x: 30, y: 100, width: view.width-60, height: view.height-300)
+        box.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 0.3)
+        
         session.startRunning()
         
+    }
+    
+    func update(corners:[CGPoint]) {
+        print("Box~~")
+        
+        
+        
+        if box.layer.sublayers != nil {
+            for la in box.layer.sublayers! {
+                la.removeFromSuperlayer()
+            }
+        }
+        
+        let path = UIBezierPath.init()
+        let start = CGPoint.init(x: (1-corners[0].y)*box.width, y: corners[0].x*box.height)
+        path.move(to: start)
+        for i in 1..<corners.count {
+            //
+            let p = CGPoint.init(x: (1-corners[i].y)*box.width, y: corners[i].x*box.height)
+            path.addLine(to: p)
+        }
+        path.addLine(to: start)
+        path.lineWidth = 1
+        
+        let boxlayer = CAShapeLayer.init()
+        boxlayer.path = path.cgPath
+        boxlayer.lineWidth = 4
+        boxlayer.strokeColor = UIColor.init(red: 0.6, green: 0.9, blue: 0.1, alpha: 0.8).cgColor
+        boxlayer.fillColor = UIColor.init(red: 0.6, green: 0.9, blue: 0.1, alpha: 0.8).cgColor
+        
+        box.layer.addSublayer(boxlayer)
     }
     
     //MARK: delegate
@@ -87,13 +123,23 @@ class QRCodeViewController: BaseViewController,AVCaptureMetadataOutputObjectsDel
         if metadataObjects.count > 0 {
 //            session.stopRunning()
             let metadata = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-            print(metadata)
-            
+
+            var points:[CGPoint] = []
+            for c in metadata.corners {
+                let p = CGPoint.init(dictionaryRepresentation: c as! CFDictionary)
+                points.append(p!)
+            }
+            update(corners: points)
+            //
             let data = metadata.stringValue
             
-            AlertModule.showToast(title: nil, conetent: data, from: self, duration: 1, complate: {
-                //
-            })
+            if !isShow {
+                isShow = true
+                AlertModule.showToast(title: nil, conetent: data, from: self, duration: 1, complate: {
+                    //
+                    self.isShow = false
+                })
+            }
         }
     }
 }
